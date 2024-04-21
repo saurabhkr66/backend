87fc563c1322160325973315cb604ce1044fc739
saurabhkr66 <sauravkum420@gmail.com>
2024-04-21 09:12:36 +0530

    postman setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv"
 import connectDB from './db/index.js';
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectDB()
 .then(()=>{
-    application.listen(process.env.PORT||8000,()=>{
+    app.listen(process.env.PORT||8000,()=>{
         console.log(`server is running at port :${process.env.PORT}`);
     })
 })
